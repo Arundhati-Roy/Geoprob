36ce7dbcd042a06dd8a89d36e13cb42023a40db6
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-09-28 09:06:39 +0530

    [AR] Refractor. Commit

--- Geoprob/Program.cs
+++ Geoprob/Program.cs
@@ -6,7 +6,12 @@ namespace Geoprob
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 3, x2 = 6;
+            int y1 = 4, y2 = 9;
+            double length = 0;
+
+            length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            Console.WriteLine("Length of the line is " + length);
         }
     }
 }
