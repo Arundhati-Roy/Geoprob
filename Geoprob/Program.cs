165dc2f3847083a11f183837f9d5435a734764aa
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-09-28 15:57:26 +0530

    [AR] First Commit

--- Geoprob/Program.cs
+++ Geoprob/Program.cs
@@ -6,12 +6,7 @@ namespace Geoprob
     {
         static void Main(string[] args)
         {
-            int x1 = 3, x2 = 6;
-            int y1 = 4, y2 = 9;
-            double length = 0;
-
-            length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("Length of the line is " + length);
+            Console.WriteLine("Welcome to Line comparison problem");
         }
     }
 }
