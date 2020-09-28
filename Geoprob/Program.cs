5be0cdc7e49cbd03cd2b3ab74cf27ad6632ddf50
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-09-28 16:05:51 +0530

    [AR] Equality Check

--- Geoprob/Program.cs
+++ Geoprob/Program.cs
@@ -8,10 +8,21 @@ namespace GeoAssg1
         {
             int x1 = 3, x2 = 6;
             int y1 = 4, y2 = 9;
-            double length = 0;
 
-            length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("Length of the line is " + length);
+            int x3 = 5, x4 = 10;
+            int y3 = 8, y4 = 12;
+            double length1 = 0, lenght2 = 0;
+
+            length1 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            lenght2 = Math.Sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+            if (lenght2.Equals(length1))
+            {
+                Console.WriteLine("Both lines are of equal length");
+            }
+            else
+            {
+                Console.WriteLine("Both lines are of different length");
+            }
         }
     }
 }
